a63a36ac5fe01f6f136e8ec68fed3a2812d30dcb
darshan3751 <darshanih2057@gmail.com>
2026-04-11 19:56:37 +0530

    added staging and clean transformation

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import requests
+import duckdb
+import pandas as pd
 
 def get_pokemon_data(pokemon_id):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
     data = requests.get(url).json()
 
     return {
+        "id": data["id"],
         "name": data["name"],
         "height": data["height"],
         "weight": data["weight"]
     }
 
+# Fetch data
 pokemon = get_pokemon_data(1)
 
-print("Pokemon Details")
-print("----------------")
-print("Name:", pokemon["name"])
-print("Height:", pokemon["height"])
-print("Weight:", pokemon["weight"])
+# Convert to DataFrame
+df = pd.DataFrame([pokemon])
+
+# Connect to DuckDB
+con = duckdb.connect("pokemon.db")
+
+# Create table and insert data
+con.execute("CREATE TABLE IF NOT EXISTS staging_pokemon AS SELECT * FROM df")
+con.execute("""
+CREATE TABLE IF NOT EXISTS clean_pokemon AS
+SELECT 
+    id,
+    UPPER(name) AS name,
+    height,
+    weight,
+    CASE 
+        WHEN weight > 100 THEN 'Heavy'
+        ELSE 'Light'
+    END AS weight_category
+FROM staging_pokemon
+""")
+
+# Query data
+result = con.execute("SELECT * FROM clean_pokemon").fetchdf()
+
+print("Clean Data:")
+print(result)

3359d64bf7d73fee9acf8d748d36ef73e56cb531
darshan3751 <darshanih2057@gmail.com>
2026-04-12 13:27:32 +0530

    advanced analysis and more features

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def get_pokemon_data(pokemon_id):
     data = requests.get(url).json()
 
     return {
-        "id": data["id"],
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"]
-    }
+    "id": data["id"],
+    "name": data["name"],
+    "height": data["height"],
+    "weight": data["weight"],
+    "base_experience": data["base_experience"],
+    "type": data["types"][0]["type"]["name"]
+}
 
 # Fetch data
 pokemon_list = []
@@ -29,13 +31,17 @@ con = duckdb.connect("pokemon.db")
 con.execute("CREATE TABLE IF NOT EXISTS staging_pokemon AS SELECT * FROM df")
 
 # Clean table (transformation)
+con.execute("DROP TABLE IF EXISTS clean_pokemon")
+
 con.execute("""
-CREATE TABLE IF NOT EXISTS clean_pokemon AS
+CREATE TABLE clean_pokemon AS
 SELECT 
     id,
     UPPER(name) AS name,
     height,
     weight,
+    base_experience,
+    type,
     CASE 
         WHEN weight > 100 THEN 'Heavy'
         ELSE 'Light'
@@ -51,3 +57,43 @@ print(result)
 
 result.to_csv("pokemon_output.csv", index=False)
 print("CSV file created!")
+
+# avg experience by type
+result1 = con.execute("""
+SELECT 
+    type,
+    AVG(base_experience) as avg_exp
+FROM clean_pokemon
+GROUP BY type
+ORDER BY avg_exp DESC
+""").fetchdf()
+
+print("\nAverage Experience by Type:")
+print(result1)
+
+# heviest pokemon
+result2 = con.execute("""
+SELECT 
+    type,
+    MAX(weight) as max_weight
+FROM clean_pokemon
+GROUP BY type
+""").fetchdf()
+
+print("\nHeaviest Pokemon per Type:")
+print(result2)
+
+# top 5 strong pokemon
+result3 = con.execute("""
+SELECT 
+    name,
+    base_experience
+FROM clean_pokemon
+ORDER BY base_experience DESC
+LIMIT 5
+""").fetchdf()
+
+print("\nTop 5 Strongest Pokemon:")
+print(result3)
+
+result3.to_csv("top_pokemon.csv", index=False)
